5941fa849ad609f8d3c4a7b1bdcc6886bd57e8c3
Desmond <desmondzkx@gmail.com>
2024-10-23 20:58:56 -0400

    Update database, dashboard, main controller and workflow

--- src/public/dashboard_mp.php
+++ src/public/dashboard_mp.php
@@ -2,4 +2,46 @@
 
 ?>
 
-<p>Hello MP</p>
+<?php include '../views/header.php' ?>
+<div>
+    <div>
+        <div>
+            <h1>My Bills</h1>
+            <a href="">
+                <button type="button">Create</button>
+            </a>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+
+    <div>
+        <div>
+            <h1>Voting Bills</h1>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php include '../views/footer.php' ?>
