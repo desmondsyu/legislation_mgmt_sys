ca5c363764b8da6af061e94f34465a5d8659aecd
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2024-10-25 22:53:28 -0400

    approve and reject bills for reviewer

--- src/public/dashboard_reviewer.php
+++ src/public/dashboard_reviewer.php
@@ -1,5 +1,33 @@
 <?php
+session_start();
+require_once '../config/database.php';
+require_once '../controllers/BillController.php';
+$billController = new BillController($pdo);
+$errorMessage = "";
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($_POST["bill"]) || empty($_POST["action"])) {
+        $errorMessage .= "<p>Please select bill!</p>";
+    } else {
+        $billId = $_POST['bill'];
+        $action = $_POST['action'];
+        try {
+            switch ($action) {
+                case "reject":
+                    $billController->rejectBill($billId);
+                    break;
+                case "approve":
+                    $billController->approveBill($billId);
+                    break;
+                default:
+                    break;
+            }
+            header("location: dashboard_reviewer.php");
+        } catch (Exception $e) {
+            $errorMessage .= "<p>" . $e->getMessage() . "</p>";
+        }
+    }
+}
 ?>
 
 <?php include '../views/header.php' ?>
@@ -18,7 +46,28 @@
                 </tr>
             </thead>
             <tbody>
-
+            <?php
+                $billsToReview = $billController->findByStatus('R');
+                foreach ($billsToReview as $bill) {
+                    echo "<tr>
+                                        <td>{$bill['title']}</td>
+                                        <td>{$bill['description']}</td>
+                                        <td>{$bill['status']}</td>
+                                        <td>
+                                            <form method='post'>
+                                                <input type='hidden' name='bill' value='{$bill['id']}'>
+                                                <input type='hidden' name='action' value='reject'>
+                                                <input type='submit' name='submit' value='Reject'>
+                                            </form> 
+                                            <form method='post'>
+                                                <input type='hidden' name='bill' value='{$bill['id']}'>
+                                                <input type='hidden' name='action' value='approve'>
+                                                <input type='submit' name='submit' value='Approve'>
+                                            </form>     
+                                        </td>
+                                      </tr>";
+                }
+                ?>
             </tbody>
         </table>
     </div>
