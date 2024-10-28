c15b5d0b7a2bac8382468435ed07c6b4c7e4dc95
Desmond <desmondzkx@gmail.com>
2024-10-28 16:45:56 -0400

    Fix display in form

--- src/views/bill_form.php
+++ src/views/bill_form.php
@@ -1,11 +1,11 @@
 <label for="title">Title</label>
-<input type="text" name="title" id="title"  value=<?php echo $title; ?> <?php echo $readonly ? 'readonly' : ''; ?>>
+<input type="text" name="title" id="title"  value="<?php echo $title; ?>" <?php echo $readonly ? 'readonly' : ''; ?>>
 <br>
 
 <label for="description">Description</label>
-<input type="text" name="description" id="description" value=<?php echo $description ?> <?php echo $readonly ? 'readonly' : ''; ?>>
+<input type="text" name="description" id="description" value="<?php echo $description ?>" <?php echo $readonly ? 'readonly' : ''; ?>>
 <br>
 
 <label for="content">Content</label>
-<input type="text" name="content" id="content" value=<?php echo $content?> <?php echo $readonly ? 'readonly' : ''; ?>>
+<input type="text" name="content" id="content" value="<?php echo $content?>" <?php echo $readonly ? 'readonly' : ''; ?>>
 <br>
